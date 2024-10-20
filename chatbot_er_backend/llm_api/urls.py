c8ac0198e60e1e443b9c73bfc4d79f9f2cfa252b
S0brina <118025166+S0brina@users.noreply.github.com>
2024-10-19 22:05:24 -0500

    Delete chatbot_er_backend directory

--- chatbot_er_backend/llm_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import generate_message
-
-urlpatterns = [
-    #path('generate/', generate_llm_response, name='generate_llm_response'),
-    path('generate/', generate_message, name='generate_message'), 
-]
